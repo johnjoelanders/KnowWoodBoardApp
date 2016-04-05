df35f9640f0a37e749e0f0e9505d936d00d1b5e6
LukeBluett <lukebluett@mail.com>
2016-04-05 12:58:48 +0100

    Created method getWordToGuess that chooses a word from an array. Created variables in GameActivity for the onCreate method that call methods getWordToGuess and getRandomNumberFromArrayIndex

--- app/src/main/java/com/example/john/knowwoodboardapp/GameActivity.java
+++ app/src/main/java/com/example/john/knowwoodboardapp/GameActivity.java
@@ -7,15 +7,24 @@ import java.util.Random;
 
 public class GameActivity extends AppCompatActivity {
 
+    private String[] sampleArray = {"hello", "johnjoe", "and adele", "its me"};
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_game);
 
+        int index = getRandomNumberForArrayIndex(sampleArray.length);
+        String wordToGuess = getWordToGuess(index);
     }
 
     private int getRandomNumberForArrayIndex(int range) {
         Random rand = new Random();
         return rand.nextInt(range);
     }
+
+    private String getWordToGuess(int index) {
+        return sampleArray[index];
+    }
+
 }
