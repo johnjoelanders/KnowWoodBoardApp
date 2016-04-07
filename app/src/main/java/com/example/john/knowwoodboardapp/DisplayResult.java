873864c1b229023dc5fcbe9dbcc57fd381f0078f
LukeBluett <lukebluett@mail.com>
2016-04-07 12:22:56 +0100

    Added constructor to initialize all the parameters. Added a method to convert wordToGuess into a hidden word to allow the amount of characters to be displayed without displaying text.

--- app/src/main/java/com/example/john/knowwoodboardapp/DisplayResult.java
+++ app/src/main/java/com/example/john/knowwoodboardapp/DisplayResult.java
@@ -1,5 +1,6 @@
 package com.example.john.knowwoodboardapp;
 
+import java.util.ArrayList;
 import java.util.List;
 
 /**
@@ -10,8 +11,24 @@ public class DisplayResult {
     private List<String> wordToGuess;
     private List<String> wordToGuessHidden;
     private List<String> incorrectUsedLetters;
+    private int attemptsLeft;
 
-    public DisplayResult() {
+    public DisplayResult(ArrayList<String> wordToGuess) {
+        this.wordToGuess = wordToGuess;
+        incorrectUsedLetters = new ArrayList<String>();
+        attemptsLeft = 10;
+        wordToGuessHidden = hideWordWithDashes(wordToGuess);
+    }
 
+    public ArrayList<String> hideWordWithDashes(ArrayList<String> wordToGuess) {
+        ArrayList<String> hiddenWord = new ArrayList<String>();
+        for(String i : wordToGuess) {
+            if(i.equals(" ")) {
+                hiddenWord.add("   ");
+            } else {
+                hiddenWord.add("-");
+            }
+        }
+        return hiddenWord;
     }
 }
