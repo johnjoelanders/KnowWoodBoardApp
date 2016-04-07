302b4d2611fc41309fd72b9745156f9413dbdc56
Johnjoe Landers <johnjoelanders@outlook.com>
2016-04-07 09:20:55 +0100

    Created databasehandler.java which will deal with all database methods we need. Created a sqlite database called Knowoodboard.db with one table called highscores which takes an ID,Name and Score.

---
+++ app/src/main/java/com/example/john/knowwoodboardapp/DatabaseHandler.java
@@ -0,0 +1,30 @@
+package com.example.john.knowwoodboardapp;
+
+import android.content.Context;
+import android.database.sqlite.SQLiteDatabase;
+import android.database.sqlite.SQLiteOpenHelper;
+
+/**
+ * Created by John on 07/04/2016.
+ */
+public class DatabaseHandler extends SQLiteOpenHelper{
+    public static final String DATABASE_NAME ="Knowwoodboard.db";
+    public static final String TABLE_NAME ="highscores";
+    public static final String COL_1 ="ID";
+    public static final String COL_2 ="Name";
+    public static final String COL_3 ="Score";
+
+    public DatabaseHandler(Context context, String name, SQLiteDatabase.CursorFactory factory, int version) {
+        super(context, DATABASE_NAME, null, 1);
+    }
+
+    @Override
+    public void onCreate(SQLiteDatabase db) {
+
+    }
+
+    @Override
+    public void onUpgrade(SQLiteDatabase db, int oldVersion, int newVersion) {
+
+    }
+}
