4879b556ed18280fea87157eab8822305ea71e29
LukeBluett <lukebluett@mail.com>
2016-04-07 11:57:02 +0100

    Created DisplayResult class to handle the dynamic information to be displayed on GameActivity

---
+++ app/src/main/java/com/example/john/knowwoodboardapp/DisplayResult.java
@@ -0,0 +1,17 @@
+package com.example.john.knowwoodboardapp;
+
+import java.util.List;
+
+/**
+ * Created by LBluett on 07/04/16.
+ */
+public class DisplayResult {
+
+    private List<String> wordToGuess;
+    private List<String> wordToGuessHidden;
+    private List<String> incorrectUsedLetters;
+
+    public DisplayResult() {
+
+    }
+}
