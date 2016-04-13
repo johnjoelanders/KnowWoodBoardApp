b33e4f27da52c86a520f0f69fd4d90e69eda4b02
LukeBluett <lukebluett@mail.com>
2016-04-13 11:18:08 +0100

    Added methods in GameActivity for skeleton code

--- app/src/main/java/com/example/john/knowwoodboardapp/GameActivity.java
+++ app/src/main/java/com/example/john/knowwoodboardapp/GameActivity.java
@@ -5,6 +5,8 @@ import android.support.v7.app.AppCompatActivity;
 import android.view.View;
 import android.widget.Button;
 
+import java.util.ArrayList;
+import java.util.Arrays;
 import java.util.List;
 
 public class GameActivity extends AppCompatActivity {
@@ -32,10 +34,6 @@ public class GameActivity extends AppCompatActivity {
         return wordToGuessHidden;
     }
 
-    private void btnSubmitOnClick() {
-
-    }
-
     private String formatArraylistToString(List<String> listToFormat) {
         return listToFormat.toString()
                 .replace(",", " ")
@@ -44,5 +42,17 @@ public class GameActivity extends AppCompatActivity {
                 .trim();
     }
 
+    private List<String> formatStringToArraylist(String stringToFormat) {
+        String[] strValues = stringToFormat.split("");
 
+        return new ArrayList<String>(Arrays.asList(strValues));
+    }
+
+    private String wordToGuess() {
+        return "tree";
+    }
+
+    private void btnSubmitOnClick() {
+
+    }
 }
