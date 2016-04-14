99cb289ebe0dd7abf461a389766f8eced680dd16
Johnjoe Landers <johnjoelanders@outlook.com>
2016-04-14 10:20:24 +0100

    Updated creating hangman change live to go from 9 - 0 instead of 10 to 1 also changed the colour of the hangman to blue.

--- app/src/main/java/com/example/john/knowwoodboardapp/CreatingHangman.java
+++ app/src/main/java/com/example/john/knowwoodboardapp/CreatingHangman.java
@@ -5,9 +5,7 @@ import android.graphics.Canvas;
 import android.graphics.Color;
 import android.graphics.Paint;
 import android.util.DisplayMetrics;
-import android.view.Display;
 import android.view.View;
-import android.view.WindowManager;
 import android.widget.TextView;
 
 /**
@@ -28,49 +26,50 @@ public class CreatingHangman extends View{
     public void onDraw(Canvas canvas){
         super.onDraw(canvas);
         //int lives = Integer.parseInt(text);
-        int lives = 5;
+        int lives = 0;
         int width = lDisplayMetrics.widthPixels;
         int height = lDisplayMetrics.heightPixels;
-        paint.setColor(Color.BLACK);
         paint.setStrokeWidth(10);
 
-        if(lives <= 10){
+        if(lives <= 9){
             //Draw Base
+            paint.setColor(Color.BLACK);
             canvas.drawLine(width/4,height/3,width/2,height/3,paint);
         }
-        if(lives <= 9){
+        if(lives <= 8){
             //Draw pillar
             canvas.drawLine(width/4,height/3,width/4,height/14,paint);
         }
-        if(lives <= 8){
+        if(lives <= 7){
             //Draw top Horizontal line
             canvas.drawLine(width/4,height/14,width/2,height/14,paint);
         }
-        if(lives <= 7){
+        if(lives <= 6){
             //Draw rope
             canvas.drawLine(width/2,height/14,width/2,height/10,paint);
         }
-        if(lives <= 6){
+        if(lives <= 5){
             //Draw Head
+            paint.setColor(Color.BLUE);
             canvas.drawCircle(width/2,height/8,width/14,paint);
         }
-        if(lives <= 5){
+        if(lives <= 4){
             //Draw Body
             canvas.drawLine(width/2,height/8,width/2,height/4,paint);
         }
-        if(lives <= 4){
+        if(lives <= 3){
             //Draw Left arm
             canvas.drawLine(width/2,height/6,width/2-30,height/4,paint);
         }
-        if(lives <= 3){
+        if(lives <= 2){
             //Draw Right Arm
             canvas.drawLine(width/2,height/6,width/2+30,height/4,paint);
         }
-        if(lives <= 2){
+        if(lives <= 1){
             //Draw Left Leg
             canvas.drawLine(width/2,height/4,width/2-30,height/3-15,paint);
         }
-        if(lives <= 1){
+        if(lives <= 0){
             //Draw Right leg end game
             canvas.drawLine(width/2,height/4,width/2+30,height/3-15,paint);
         }
